a98fe117f5f9f6eb3cf277ac04e2b9b5781a511b
rafald121 <rafald121@gmail.com>
2017-04-24 16:13:40 +0200

    create MovieViewHolder class but not implemented

---
+++ app/src/main/java/com/example/admin/myapplication/ViewHolders/MovieViewHolder.java
@@ -0,0 +1,18 @@
+package com.example.admin.myapplication.ViewHolders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.admin.myapplication.R;
+import com.example.admin.myapplication.Utils.NetworkHelper;
+
+/**
+ * Created by admin on 24.04.2017.
+ */
+
+public class MovieViewHolder {
+
+}
