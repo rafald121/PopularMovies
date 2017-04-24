178a6795acaf424f1fd4c35580091497956571de
rafald121 <rafald121@gmail.com>
2017-04-24 15:32:18 +0200

    add separate package for own Listeners

---
+++ app/src/main/java/com/example/admin/myapplication/Interfaces/MovieReviewClickListener.java
@@ -0,0 +1,9 @@
+package com.example.admin.myapplication.Interfaces;
+
+/**
+ * Created by admin on 24.04.2017.
+ */
+
+public interface MovieReviewClickListener {
+    void movieReviewClickListener(String idFromNet);
+}
