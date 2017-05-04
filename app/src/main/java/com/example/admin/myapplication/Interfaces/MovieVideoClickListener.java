f0d6a45a5eda4068169b9d0cd72f54a1cb72bb61
rafald121 <rafald121@gmail.com>
2017-05-04 14:47:46 +0200

    add MovieVideoClickListener

---
+++ app/src/main/java/com/example/admin/myapplication/Interfaces/MovieVideoClickListener.java
@@ -0,0 +1,9 @@
+package com.example.admin.myapplication.Interfaces;
+
+/**
+ * Created by admin on 04.05.2017.
+ */
+
+public interface MovieVideoClickListener {
+    void onClickMovieVideoListener(String key);
+}
