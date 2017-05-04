f7bcc883f3b2a0157e10cc672ea6a4801a50a8a7
rafald121 <rafald121@gmail.com>
2017-05-05 01:29:14 +0200

    add interfaces to check whole review content

---
+++ app/src/main/java/com/example/admin/myapplication/Interfaces/MovieReviewSiteClickListener.java
@@ -0,0 +1,9 @@
+package com.example.admin.myapplication.Interfaces;
+
+/**
+ * Created by admin on 05.05.2017.
+ */
+
+public interface MovieReviewSiteClickListener {
+    public void onReviewClicked(String url);
+}
